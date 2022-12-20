2336a9e1c58666a449e434b56b7b5996fac873ad
AlexandreGRN <tulkiidra@gmail.com>
2022-12-20 11:59:23 +0000

    Adding Define-like statement + if tests using ages

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+public class Constants {
+    public static final int INVALID_VALUE = 2;
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World\n");
+        int myAge = -50000;
+        int herAge = 30;
+
+        if (myAge < 0 || herAge < 0) {
+            System.out.println("Invalid Age");
+            System.out.println(Constants.INVALID_VALUE);
+            System.exit(Constants.INVALID_VALUE);
+        }
+        if (myAge == herAge) {
+            System.out.println("Same age");
+        }
+        else if (myAge > herAge) {
+            System.out.println("Older than her");
+        }
+        else {
+            System.out.println("Younger than her");
+        }
     }
 }
